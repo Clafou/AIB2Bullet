2e64f3dced93f617f9da4b63a1d9db62498d21c3
Sébastien Molines <clafou@gmail.com>
2019-05-01 19:34:15 +0100

    Initial commit

---
+++ convert.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const parse = require('csv-parse');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const args = process.argv;
+
+if (args.length < 3) {
+  throw 'Input file not provided';
+}
+if (args.length < 4) {
+  throw 'Output file not provided';
+}
+const inputFile = args[2];
+const outputFile = args[3];
+
+console.log('Converting ' + inputFile);
+var rows = [];
+fs.createReadStream(inputFile)
+.pipe(parse())
+.on('data', function(data) {
+    if (data[0] === 'Posted Account') {
+      console.log('Skipped: Header');
+      return
+    }
+    let row = {
+        date: data[1],
+        description: data[2],
+        moneyOut: data[3],
+        moneyIn: data[4],
+    }
+    if (row.moneyIn == '' && row.moneyOut == '') {
+      console.log('Skipped: ' + JSON.stringify(row))  // Skip information rows (without in or out amounts)
+      return
+    }
+    rows.push(row)
+})
+.on('end', function() {
+    console.log('Converted: ' + JSON.stringify(rows, null, 2))
+    createCsvWriter({
+      path: outputFile,
+      header: [
+        {id: 'date', title: 'Date'},
+        {id: 'description', title: 'Description'},
+        {id: 'moneyOut', title: 'Money Out'},
+        {id: 'moneyIn', title: 'Money In'}
+      ]
+    })
+    .writeRecords(rows)
+    .then(()=> console.log(`Conversion to file ${ outputFile } completed successfully`));
+})
---
+++ readme.md
@@ -0,0 +1,17 @@
+# AIB Internet Banking to Bullet CSV conversion
+
+Converts a CSV file exported by AIB Internet Banking to a CSV file that can be imported by [Bullet](https://www.bullethq.com)
+
+## Installation
+
+[npm][]:
+
+```bash
+npm install
+```
+
+## Usage
+
+```bash
+node index.js path/to/aib/file.csv path/to/output/file.csv
+```
